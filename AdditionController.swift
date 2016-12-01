7c72c4a97a713ab2d8e96139523759279e89414c
Spursh <spursh.ujjawal@sjsu.edu>
2016-12-01 01:13:47 -0800

    correct structure

---
+++ AdditionController.swift
@@ -0,0 +1,36 @@
+//
+//  AdditionController.swift
+//  MathQuiz
+//
+//  Created by Student on 11/30/16.
+//  Copyright © 2016 Student. All rights reserved.
+//
+
+import UIKit
+
+class AdditionController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("addition")
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
