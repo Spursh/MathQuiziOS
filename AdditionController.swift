6c88739777648a13d387d892526edd3a150a8ca1
Spursh <spursh.ujjawal@sjsu.edu>
2016-12-01 01:23:49 -0800

    all screen are now reachable

--- AdditionController.swift
+++ AdditionController.swift
@@ -20,6 +20,7 @@ class AdditionController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        print("addition1")
     }
     
 
